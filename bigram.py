66a7bcca4e0fac5a6be78c813ec0c00107fa8192
Manoj <manojgrg314@gmail.com>
2024-10-20 01:40:06 -0500

    Fixed final layer normalization

--- bigram.py
+++ bigram.py
@@ -176,6 +176,7 @@ class BigramLanguageModel(nn.Module):
 
         x = tok_emb + pos_emb # (B, T, C)
         x = self.blocks(x) # (B, T, C)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
